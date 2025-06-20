977797155e18099b180c89825c6872b9893f7751
Emily Quevedo <emilyqdrosa@gmail.com>
2025-06-20 20:46:22 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+public class Main {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("WICKED");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        System.out.println("Duração do Filme: " + meuFilme.getDuracaoEmMinutos());
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+        Serie serie = new Serie();
+        serie.setNome("Happiness");
+        serie.setAnoDeLancamento(2021);
+        serie.exibeFichaTecnica();
+        serie. setTemporadas(1);
+        serie.setEpisodiosPorTemporada(12);
+        serie.setMinutosPorEpisodio(50);
+        System.out.println("Duração de " + serie.getNome() + ": " + serie.getDuracaoEmMinutos() + " minutos");
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(serie);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(serie);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+    }
+}
